f0708bf4a05adc0ea08107bd176032941ce8529d
C0deZLee <c0dezlee@gmail.com>
2016-05-18 14:32:01 -0400

    init push

---
+++ app/models/course.server.model.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema,
+	crypto = require('crypto');
+
+/**
+ * User Schema
+ */
+var CourseSchema = new Schema({
+  number: {
+		type: String
+	},
+  name: {
+		type: String
+	},
+	fullName: {
+		type: String
+	},
+  section: {
+    type: String
+  },
+	instructor1: {
+		type: String
+	},
+  instructor2: {
+    type: String
+	},
+	unit: {
+		type: String
+	},
+  room: {
+    type: String
+  },
+	time: {
+		type: String
+	},
+  status: {
+    type: String
+  },
+  capacity: {
+    type: Number
+  },
+  enrolled: {
+    type: Number
+  },
+  waitlist: {
+    type: Number
+  },
+  waitlistEnrolled: {
+    type: Number
+  }
+});
+mongoose.model('Course', CourseSchema);
