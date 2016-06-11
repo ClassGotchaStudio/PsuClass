26901ab3455719ad71538a0a446c3ab20b6f56c2
C0deZLee <c0dezlee@gmail.com>
2016-06-11 18:33:45 -0400

    beta view

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -6,8 +6,10 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
 
-        // Some example string
-        $scope.helloText = 'Welcome in INSPINIA MEAN.JS Boilerplate';
-        $scope.descriptionText = 'It is an application skeleton for a typical MEAN web app. You can use it to quickly bootstrap your project.';
+		// Get Search data
+		$scope.search = function() {
+			
+		}
+		// Show data
 	}
 ]);
